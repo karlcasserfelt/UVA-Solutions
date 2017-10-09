2939a8ec91fe29715ec72cf75b18146c3dd9e029
Karl Casserfelt <karl.casserfelt@gmail.com>
2017-10-09 18:38:38 +0200

    101 started

---
+++ UVA101/src/Main.java
@@ -0,0 +1,21 @@
+/**
+ * Created by Karl on 2017-10-09.
+ */
+public class Main {
+
+
+    public Main(){
+
+    }
+
+    public static void main(String[] args) {
+        new Main();
+    }
+
+
+    class Block{
+
+
+
+    }
+}
